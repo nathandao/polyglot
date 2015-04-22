f3024c990d23a44d1716f8fc69e8122d429cb778
Nathan Dao <nathan@exove.fi>
2015-04-22 22:12:21 +0300

    fix language model bug and increase neo4j retry count

--- app/models/language.rb
+++ app/models/language.rb
@@ -6,7 +6,6 @@ class Language
 
   has_many :in, :words, rel_class: BelongsTo
   has_many :in, :phrases, rel_class: BelongsTo
-  has_many :in, :sites, rel_class: Uses
 
   validates_presence_of :url, :name
   validates_uniqueness_of :url, :name, case_sensitive: false
